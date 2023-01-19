03f81bc48dd7e9fa8ca7d707fb2b5f969f7960d0
KhanhAn <khanhan.maicogroup@gmail.com>
2023-01-20 00:27:41 +0700

    setup Tailwind

--- karaoke-nice-listing/src/App.js
+++ karaoke-nice-listing/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='text-amber-400'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
