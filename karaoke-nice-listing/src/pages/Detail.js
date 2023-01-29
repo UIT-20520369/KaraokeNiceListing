efeb22ca52831a097dd1ebc90bffe115e5507641
KhanhAn <khanhan.maicogroup@gmail.com>
2023-01-29 21:43:11 +0700

    Reuseable Dropdown Listing page with filter apply api to project Detail Page

---
+++ karaoke-nice-listing/src/pages/Detail.js
@@ -0,0 +1,12 @@
+import React,{useState,useEffect} from "react";
+import { useQuery } from "@apollo/client";
+
+const DetailRoom = ()=>{
+    return(
+       <div>
+        <input className="bg-black text-white hidden" type='datetime-local'></input>
+       </div>
+    )
+}
+
+export default DetailRoom;
